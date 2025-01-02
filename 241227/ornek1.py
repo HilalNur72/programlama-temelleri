d6e04fe3262fb4b4c6a52e836e7b94944537ffed
HilalNur72 <hilalnursaci@gmail.com>
2025-01-02 16:25:38 +0300

    Add files via upload

---
+++ 241227/ornek1.py
@@ -0,0 +1,4 @@
+su_miktarı=int(input("su_miktarını girin: ")) 
+birim_fiyat=20
+islem(su_miktarı*birim_fiyat)
+print("fatura tutarınız",islem )
---
+++ 241227/ornek2.py
@@ -0,0 +1,6 @@
+not1=int(input("not1 giriniz: "))
+not2=int(input("not2 giriniz: "))
+performans1=int(input("performans1 giriniz: "))
+performans2=int(input("performans2 giriniz: "))
+ortalama=(not1+not2+performans1+performans2)/4
+print("ders notu ortalamanız",ortalama)

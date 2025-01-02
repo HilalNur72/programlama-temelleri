a66546b7d2c60fd8763b3017d4c6af150206b6f8
HilalNur72 <hilalnursaci@gmail.com>
2025-01-02 16:25:17 +0300

    Add files via upload

---
+++ 241220/ornek2.py
@@ -0,0 +1,6 @@
+kisa_kenar=int(input(" kısa kenarını yazınız: "))
+uzun_kenar=int(input(" uzun kenarını yazınız: "))
+cevre=kisa_kenar*uzun_kenar
+alan=(kisa_kenar+uzun_kenar)*2
+print("dikdörtgenin cevresi: ",cevre )
+print("dikdörtgenin alanı:  ",alan)
